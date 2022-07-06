0dc7af1808bdf4c763e9c0d0b61f4d1a3979e0d4
sheriffkule <zeljko225@yahoo.com>
2022-07-06 19:13:32 +0200

    layouts and colors

--- App.js
+++ App.js
@@ -1,60 +1,103 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.dummyText}>Ovaj text je na prvom mestu</Text>
+        <Text style={styles.prvi}>Ovaj text je na prvom mestu.</Text>
+        <View>
+          <Text style={styles.prvidodatak}>ovo je dodatak na prvi.</Text>
+        </View>
       </View>
       <View>
-        <Text style={styles.nesto}>Zdravo Micule!</Text>
+        <Text style={styles.drugi}>Ovo je drugi text.</Text>
       </View>
       <View>
-        <Text style={styles.drugi}> ovo je drugi</Text>
+        <Text style={styles.treci}>Zdravo Micule!</Text>
       </View>
       <View>
-        <Text style={styles.cetvrti}>CETVRTI!!!</Text>
+        <Text style={styles.cetvrti}>I na kraju evo ga i CETVRTI!!!</Text>
       </View>
-
-      <Button title="Tap me!" />
+      <Button
+        style={styles.roundedButton}
+        title="Klikni"
+        onPress={() => {
+          Alert.alert('Bravo, pritisnuo si dugme, bas si sjajan!');
+          <Text style={styles.buttonText}>continue</Text>;
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
-    backgroundColor: '#000054',
-    alignItems: 'center',
+    flex: 1,
+    backgroundColor: 'darkblue',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
-  dummyText: {
+  prvi: {
+    marginHorizontal: 70,
     margin: 20,
-    padding: 25,
+    padding: 15,
     borderWidth: 10,
     borderColor: '#7f007f',
     backgroundColor: '#000000',
-  },
-  nesto: {
-    margin: 10,
-    padding: 50,
-    borderWidth: 7,
-    borderColor: '#236868',
-    backgroundColor: '#000000',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    textDecorationColor: 'aqua',
+    color: 'lightgreen',
   },
   drugi: {
+    marginHorizontal: 50,
+    margin: 20,
+    padding: 20,
+    borderWidth: 10,
+    borderColor: 'yellowgreen',
+    backgroundColor: '#000000',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    textTransform: 'capitalize',
+    color: 'olivedrab',
+  },
+  treci: {
+    marginHorizontal: 30,
     margin: 20,
     padding: 25,
-    borderWidth: 2,
-    borderColor: 'white',
+    borderWidth: 10,
+    borderColor: '#236868',
     backgroundColor: '#000000',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'green',
+    fontSize: 20,
   },
   cetvrti: {
+    marginHorizontal: 10,
     margin: 20,
-    padding: 35,
-    borderWidth: 2,
-    borderColor: 'grey',
+    padding: 30,
+    borderWidth: 10,
+    borderBottomColor: 'indigo',
+    borderTopColor: 'aqua',
+    borderLeftColor: 'blue',
+    borderRightColor: 'blue',
     backgroundColor: '#000000',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'darkgreen',
+  },
+  buttonText: {
+    color: 'limegreen',
+  },
+  prvidodatak: {
+    textAlign: 'center',
+    fontWeight: '900',
+    fontSize: 25,
+    fontFamily: 'monospace',
+    fontStyle: 'italic',
+    color: 'teal',
+    alignSelf: 'center',
   },
 });
